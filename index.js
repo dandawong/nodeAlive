d68f05a2f46afae382b997b066aee920e0be0900
dandawong <dandawong@gmail.com>
2022-07-31 18:15:32 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+var CronJob = require('cron').CronJob;
+const fs = require('fs');
+const pMinute = 15;
+const dir = 'output'
+
+// Create output folder if not exist
+if (!fs.existsSync(dir)){
+  fs.mkdirSync(dir);
+}
+
+// Run job ever 15 minutes
+var job = new CronJob(
+	`*/${pMinute} * * * * *`,
+	function() {
+    let now = new Date();
+    let year = now.getFullYear()
+    let month = now.getMonth()+1
+    let date = now.getDate()
+    let filePath = year + '-' + month + '-' + date + '.txt'
+    fs.appendFile(`output/${filePath}`, now.toLocaleString() + '\n', err => {
+      if (err) {
+        console.error(err);
+      }
+      // file written successfully
+    });
+	},
+	null,
+	true,
+);
+// Use this if the 4th param is default value(false)
+job.start()
